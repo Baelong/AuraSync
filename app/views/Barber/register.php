38e7cbe8861cf05484a3eb3b8b803be2627c7996
4li_80 <mohamilyasali1947@gmail.com>
2024-04-22 22:09:19 -0400

    fixed views

---
+++ app/views/Barber/register.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Browse Barbers</title>
+      <h1>Available Barbers</h1>
+</head>
+<body>
+  <ul>
+    <li><a href="/Barber/profile">Barber 1</a></li>
+    <li><a href="/Barber/profile">Barber 2</a></li>
+    <li><a href="/Barber/profile">Barber 3</a></li>
+ </ul>
+
+<body>
+
+
