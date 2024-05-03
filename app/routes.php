267d9694abd555ab1d509c18507d846f1adec04b
Fruan Tabamo Jr <fruhamsan@gmail.com>
2024-05-03 14:56:40 -0400

    organized routes

--- app/routes.php
+++ app/routes.php
@@ -1,21 +1,27 @@
 <?php
 $this->addRoute('Authentication/index','Authentication,index');
+
 $this->addRoute('Client/login','Client,login');
-$this->addRoute('Barber/login','Barber,login');
-$this->addRoute('Client/register','Client,register');
-$this->addRoute('Barber/register','Barber,register');
-$this->addRoute('Client/securePlace' , 'ClientProfile,index');
-$this->addRoute('ClientProfile/edit_profile' , 'ClientProfile,modify');
-$this->addRoute('Client/logout' , 'Client,logout');
-$this->addRoute('Barber/logout' , 'Barber,logout');
-$this->addRoute('ClientProfile/createProfile','ClientProfile,createProfile');
-$this->addRoute('ClientProfile/index','ClientProfile,index');
-$this->addRoute('Appointment/clientAppointments','Appointment,clientAppointments');
-$this->addRoute('BarberProfile/createProfile','BarberProfile,createProfile');
-$this->addRoute('BarberProfile/index','BarberProfile,index');
-$this->addRoute('Barber/securePlace' , 'BarberProfile,index');
-$this->addRoute('BarberProfile/editProfile','BarberProfile,editProfile');
 $this->addRoute('Client/viewBarberProfile','Client,viewBarberProfile');
 $this->addRoute('Client/browse_barbers','Client,browse_barbers');
 $this->addRoute('Client/search','Client,search');
+$this->addRoute('Client/register','Client,register');
+$this->addRoute('Client/logout' , 'Client,logout');
+$this->addRoute('Client/securePlace' , 'ClientProfile,index');
+
+$this->addRoute('Barber/login','Barber,login');
+$this->addRoute('Barber/securePlace' , 'BarberProfile,index');
+$this->addRoute('Barber/register','Barber,register');
+$this->addRoute('Barber/logout' , 'Barber,logout');
+
+$this->addRoute('ClientProfile/edit_profile' , 'ClientProfile,modify');
+$this->addRoute('ClientProfile/createProfile','ClientProfile,createProfile');
+$this->addRoute('ClientProfile/index','ClientProfile,index');
+
+$this->addRoute('BarberProfile/createProfile','BarberProfile,createProfile');
+$this->addRoute('BarberProfile/index','BarberProfile,index');
+$this->addRoute('BarberProfile/editProfile','BarberProfile,editProfile');
+
+$this->addRoute('Appointment/clientAppointments','Appointment,clientAppointments');
+
 
