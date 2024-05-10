047dea2df4f6288ad5f8e446b186121ec890140e
Fruan Tabamo Jr <fruhamsan@gmail.com>
2024-05-10 14:19:57 -0400

    fixed db creation

--- aurasync.sql
+++ aurasync.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `aurasync`
 --
-
+CREATE DATABASE IF NOT EXISTS `aurasync` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci; USE `aurasync`;
 -- --------------------------------------------------------
 
 --
